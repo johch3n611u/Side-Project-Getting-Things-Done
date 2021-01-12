6250b21197d44c035c4acdd851d91b05ea32a492
johch3n611u <401450650@gapp.fju.edu.tw>
2021-01-12 22:01:40 +0800

    m

---
+++ assets/config/firebasebundle.js
@@ -0,0 +1,7 @@
+// https://blog.csdn.net/qq_42345237/article/details/82624435
+document.write('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"></script>');
+document.write('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/8.2.1/firebase-analytics.js"></script>');
+document.write('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/8.2.1/firebase-database.js"></script>');
+document.write('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/8.2.1/firebase-auth.js"></script>');
+document.write('<script type="text/javascript" src="https://www.gstatic.com/firebasejs/8.2.1/firebase-firestore.js"></script>');
+document.write('<script type="text/javascript" src="../assets/config/firebase/Environment.js"></script>');
