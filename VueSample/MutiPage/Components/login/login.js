b9ba7f008818e159ec751a751587822c2cc4dc6e
liu0912089608 <allen.liu@shinda.com.tw>
2021-02-08 15:35:42 +0800

    m

--- VueSample/MutiPage/Components/login/login.js
+++ VueSample/MutiPage/Components/login/login.js
@@ -1,16 +1,2 @@
 // 將login.html的通過JQuery ajax加載出來，然後放到template裡面，然後需要的function，也在這裡都準備好，我這裡沒有後端服務，所以ajax驗證用戶名密碼就不寫了，成功後，我就直接跳轉到user頁面，原文網址：https://kknews.cc/code/pexegmz.html
 
-const Login = function (resolve, reject) {
-    // 可以請求一個html文件，既然存放模板還是html文件存放比較好
-    $.get("components/login.html").then(function (res) {
-        resolve({
-            template: res,
-            methods: {
-                login: function () {
-                        // todo: Ajax Verification
-                        this.$router.push({ path: '/user' });
-                }
-            }
-        })
-    });
-};
